f8519af461006cf2f61f82d35b7f7f1e4318ebce
KostyaD <thedamaxstudio@gmail.com>
2013-10-29 14:57:45 +0400

    trade

--- application/views/users_interface/trade.php
+++ application/views/users_interface/trade.php
@@ -19,15 +19,38 @@
 				endif;
 			?>
 			<?php if($this->loginstatus && $this->profile['demo'] == 0):?>
-				<iframe id="trade-wrapper" src="http://live.actforex.sysfx.com:8100/trade/trade.jsp?entry=deal.184&&lang=<?=$language;?>"></iframe>
+				<iframe style="width: 100%;" id="trade-wrapper" src="http://live.actforex.sysfx.com:8100/trade/trade.jsp?entry=deal.184&&lang=<?=$language;?>"></iframe>
 			<?php else:?>
-				<iframe id="trade-wrapper" src="http://demo.actforex.sysfx.com:8100/trade/trade.jsp?entry=demo.184&lang=<?=$language;?>"></iframe>
+				<iframe style="width: 100%;" id="trade-wrapper" src="http://demo.actforex.sysfx.com:8100/trade/trade.jsp?entry=demo.184&lang=<?=$language;?>"></iframe>
 			<?php endif;?>
 			</div>
 		</div>
 		<div class="clear"></div>
-		<div>
-			<?=$content;?>
+		<div class="container_12 reg-blocks">
+			<div class="grid_3 typical-menu">
+				<nav>
+					<ul>
+						<li><a href="#" class="typical-link">Опционы ONE</a></li>
+						<li><a href="#" class="typical-link">Touch</a></li>
+						<li><a href="#" class="typical-link">Бинарные опционы</a></li>
+						<li><a href="#" class="typical-link">Почему optospot</a></li>
+						<li><a href="#" class="typical-link">Нащи особенности</a></li>
+						<li><a href="#" class="typical-link">Техника трейдинга</a></li>
+						<li><a href="#" class="typical-link"></a></li>
+						<li><a href="#" class="typical-link"></a></li>
+					</ul>
+				</nav>
+			</div>
+			<div class="grid_6">
+				<?=$content;?>
+			</div>
+			<div class="grid_3 typical-news">
+				<div class="typical-news-in">
+					<div class="typical-news-title"><img src="<?=baseURL();?>img/rbk.png"><p class="typical-link">Новости рбк</p></div>
+					<a href="#" class="typical-news-item normal-text">Дмитрий Панкин предподнес прощальный подарок финансистам</a>
+					<a href="#" class="typical-news-item normal-text">Дмитрий Панкин предподнес прощальный подарок финансистам</a>
+				</div>
+			</div>
 		</div>
 	</div>
 	<div class="clear"></div>
