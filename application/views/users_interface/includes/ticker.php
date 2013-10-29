33df416f9a708b940422d434362d509a5debeb0f
KostyaD <thedamaxstudio@gmail.com>
2013-10-29 14:49:27 +0400

    ticker

--- application/views/users_interface/includes/ticker.php
+++ application/views/users_interface/includes/ticker.php
@@ -2,8 +2,6 @@
 	<div class="money-select-div">
 		<select class="money-select">
 			<option>USD/JPY</option>
-			<option>USD/JPY</option>
-			<option>USD/JPY</option>
 		</select>
 	</div>
 	<div class="money-color-div">
@@ -12,7 +10,79 @@
 	</div>
 	<div class="money-earn-div">
 		<p class="money-earn-top"><?=$this->localization->getLocalButton('ticker','winning_payout')?><br><span>$1.65</span></p>
-		<p class="money-earn-bottom"><?=$this->localization->getLocalButton('ticker','minimum_payout')?><br><span>$0.12</span></p>
+		<p class="money-earn-bottom"><?=$this->localization->getLocalButton('ticker','minimum_payout')?><br><span>$0.15</span></p>
+	</div>
+	<div class="money-cont">
+		<img src="<?=baseURL('img/money-arrow-top.png');?>">
+		<p>82.<span>38</span></p>
+		<img src="<?=baseURL('img/money-arrow-down.png');?>">
+	</div>
+	<div class="money-submit">
+		<p>00:08</p>
+		<a href="<?=base_url('trade');?>" class="green-button"><?=$this->localization->getLocalButton('ticker','invest')?></a>
+	</div>
+</div>
+<div class="grid_9 money-div">
+	<div class="money-select-div">
+		<select class="money-select">
+			<option>USD/JPY</option>
+		</select>
+	</div>
+	<div class="money-color-div">
+		<p class="money-color"><?=$this->localization->getLocalButton('ticker','payout')?><br><span>63%</span></p>
+		<input type="text" class="money-all" placeholder="<?=$this->localization->getLocalButton('ticker','amount')?>, $">
+	</div>
+	<div class="money-earn-div">
+		<p class="money-earn-top"><?=$this->localization->getLocalButton('ticker','winning_payout')?><br><span>$1.63</span></p>
+		<p class="money-earn-bottom"><?=$this->localization->getLocalButton('ticker','minimum_payout')?><br><span>$0.15</span></p>
+	</div>
+	<div class="money-cont">
+		<img src="<?=baseURL('img/money-arrow-top.png');?>">
+		<p>106.<span>88</span></p>
+		<img src="<?=baseURL('img/money-arrow-down.png');?>">
+	</div>
+	<div class="money-submit">
+		<p>00:03:48</p>
+		<a href="<?=base_url('trade');?>" class="green-button"><?=$this->localization->getLocalButton('ticker','invest')?></a>
+	</div>
+</div>
+<div class="grid_9 money-div">
+	<div class="money-select-div">
+		<select class="money-select">
+			<option>USD/GPB</option>
+		</select>
+	</div>
+	<div class="money-color-div">
+		<p class="money-color"><?=$this->localization->getLocalButton('ticker','payout')?><br><span>57%</span></p>
+		<input type="text" class="money-all" placeholder="<?=$this->localization->getLocalButton('ticker','amount')?>, $">
+	</div>
+	<div class="money-earn-div">
+		<p class="money-earn-top"><?=$this->localization->getLocalButton('ticker','winning_payout')?><br><span>$0.63</span></p>
+		<p class="money-earn-bottom"><?=$this->localization->getLocalButton('ticker','minimum_payout')?><br><span>$1.75</span></p>
+	</div>
+	<div class="money-cont">
+		<img src="<?=baseURL('img/money-arrow-top.png');?>">
+		<p>77.<span>54</span></p>
+		<img src="<?=baseURL('img/money-arrow-down.png');?>">
+	</div>
+	<div class="money-submit">
+		<p>00:03:48</p>
+		<a href="<?=base_url('trade');?>" class="green-button"><?=$this->localization->getLocalButton('ticker','invest')?></a>
+	</div>
+</div>
+<div class="grid_9 money-div">
+	<div class="money-select-div">
+		<select class="money-select">
+			<option>USD/JPY</option>
+		</select>
+	</div>
+	<div class="money-color-div">
+		<p class="money-color blue-color"><?=$this->localization->getLocalButton('ticker','payout')?><br><span>65%</span></p>
+		<input type="text" class="money-all" placeholder="<?=$this->localization->getLocalButton('ticker','amount')?>, $">
+	</div>
+	<div class="money-earn-div">
+		<p class="money-earn-top"><?=$this->localization->getLocalButton('ticker','winning_payout')?><br><span>$1.65</span></p>
+		<p class="money-earn-bottom"><?=$this->localization->getLocalButton('ticker','minimum_payout')?><br><span>$0.15</span></p>
 	</div>
 	<div class="money-cont">
 		<img src="<?=baseURL('img/money-arrow-top.png');?>">
@@ -20,7 +90,7 @@
 		<img src="<?=baseURL('img/money-arrow-down.png');?>">
 	</div>
 	<div class="money-submit">
-		<p>00:08</p>
+		<p>00:02:27</p>
 		<a href="<?=base_url('trade');?>" class="green-button"><?=$this->localization->getLocalButton('ticker','invest')?></a>
 	</div>
 </div>
