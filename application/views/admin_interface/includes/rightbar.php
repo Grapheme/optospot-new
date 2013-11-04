b059f0703df62818b60c28f8e2e7b119f60e44d8
KostyaD <thedamaxstudio@gmail.com>
2013-11-04 16:55:27 +0400

    withdrawAL

--- application/views/admin_interface/includes/rightbar.php
+++ application/views/admin_interface/includes/rightbar.php
@@ -4,7 +4,7 @@
 			<li class="nav-header">Navigation</li>
 			<li num="home"><?=anchor('','Home page');?></li>
 			<li num="users-list"><?=anchor('admin-panel/actions/users-list','Accounts');?></li>
-			<li num="withdraw"><?=anchor('admin-panel/withdraw','Withdraw');?></li>
+			<li num="withdraw"><?=anchor('admin-panel/withdraw','Withdrawal');?></li>
 			<li num="pages"><?=anchor('admin-panel/actions/pages','Content');?></li>
 			<li num="settings"><?=anchor('admin-panel/actions/settings','Settings');?></li>
 			<li num="log"><?=anchor('admin-panel/log','Logs List');?></li>
