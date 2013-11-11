c0a74338f290eb1511c3f652ac8b95e9d116e451
KostyaD <thedamaxstudio@gmail.com>
2013-11-11 20:27:56 +0400

    awards

--- application/views/users_interface/award.php
+++ application/views/users_interface/award.php
@@ -12,6 +12,44 @@
 	<div class="award-container">
 		<h1 class="award-title">Forex Expo Awards 2013</h1>
 	</div>
+	<div class="container_12">
+		<div class="grid_12">
+			<h1 class="award-h1">- Forex Awards 2013 -</h1>
+		</div>
+		<div class="award-block-left">
+			<div class="normal-text award-text" style="margin-top: 0;">
+				Бинарные опционы – это один из самых быстрых и эффективных способов заработка в Интернете, не требующий быть асом в мире биржевого рынка или какой-либо профессиональной подготовки. Торговля опционами не требует больших стартовых вложений, но позволяет быстро заработать на финансовых изменениях фондовых рынков. Желание выиграть крупную сумму за короткий срок людей влечет на финансовые торги. К сожалению, с таким большим количеством брокеров, которые предлагают сегодня свои услуги для торговли бинарными опционами, тяжело выбрать надежную компанию для торговли на рынке.
+			</div>
+			<img src="<?=baseURL('img/award-photo-1.png');?>" style="width: 130%; margin-left: -30%;">
+			<div class="award-quote">
+			 	Желание выиграть крупную сумму за короткий срок людей влечет на финансовые торги. К сожалению, с таким большим количеством брокеров, которые предлагают сегодня свои услуги для торговли бинарными опционами, тяжело выбрать надежную компанию для торговли на рынке. 
+			</div>
+		</div>
+		<div class="award-block-right">
+			<img src="<?=baseURL('img/award-photo-2.png');?>" style="width: 130%; margin-right: -30%;">
+			<div class="normal-text award-text">
+				<p class="award-title-span">Сделайте прогноз</p>
+				К сожалению, с таким большим количеством брокеров, которые предлагают сегодня свои услуги для торговли бинарными опционами, тяжело выбрать надежную компанию для торговли на рынке.
+			</div>
+		</div>
+	</div>
+	<div class="award-items-container">
+		<div class="container_12">
+			<div class="grid_12">
+				<h1>- Награды -</h1>
+			</div>
+			<div class="award-circle-div">
+				<div class="award-circle">
+					<div class="award-diploma-1"></div>
+					<h2>Кубок<br>&laquo;Best binary option broker&raquo;</h2>
+				</div>
+				<div class="award-circle">
+					<div class="award-diploma-2"></div>
+					<h2>Диплом<br>&laquo;Best binary option broker&raquo;</h2>
+				</div>
+			</div>
+		</div>
+	</div>
 	<div class="clear"></div>
 	<?php $this->load->view("users_interface/modal/signin");?>
 	<div class="dark-screen"></div>
