7619e57d1723f5a10943a0303e4c9362c6b00171
KostyaD <thedamaxstudio@gmail.com>
2013-11-28 20:00:02 +0400

    chat for clients

--- application/views/users_interface/includes/scripts.php
+++ application/views/users_interface/includes/scripts.php
@@ -1,30 +1,4 @@
-<?php 
-if($this->language_url == "ru") {
-	$chat_lang = "ru";
-} else
-	{
-		$chat_lang = "en";
-	} 
-?>
-<script>
-	(function(){
-	// your widget ID
-	var widget_id = 654946;
-	_shcp =[{widget_id : widget_id}];
-	// set default language
-	var lang =("<?=$chat_lang;?>")
-	.substr(0,2).toLowerCase();
-	// script url
-	var url ="widget.siteheart.com/widget/sh/"+ widget_id +"/"+ lang +"/widget.js";
-	var hcc = document.createElement("script");
-	hcc.type ="text/javascript";
-	hcc.async = true;
-	hcc.src =("https:"== document.location.protocol ?"https":"http")
-	+"://"+ url;
-	var s = document.getElementsByTagName("script")[0];
-	s.parentNode.insertBefore(hcc, s.nextSibling);
-	})();
-</script>
+<?php $this->load->view("html/chat"); ?>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="<?=baseURL('js/vendor/jquery-1.10.2.min.js');?>"><\/script>')</script>
 <script src="<?=baseURL('js/vendor/jquery.liMarquee.js');?>"></script>
