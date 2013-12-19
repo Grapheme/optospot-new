ee4b4b3a195b04b89124ff10d4e85c77ad246c65
Vladimir Kharseev <vkharseev@gmail.com>
2013-12-19 15:07:49 +0400

    work

--- application/views/users_interface/trade.php
+++ application/views/users_interface/trade.php
@@ -23,7 +23,7 @@
 			<?php if($this->loginstatus && $this->profile['demo'] == 0):?>
 				<iframe id="trade-wrapper" src="http://live.actforex.sysfx.com:8100/trade/trade.jsp?entry=deal.184&&lang=<?=$language;?>"></iframe>
 			<?php else:?>
-				<iframe id="trade-wrapper" src="http://demo.actforex.sysfx.com:8100/trade/trade.jsp?entry=demo.184&lang=<?=$language;?>"></iframe>
+				<iframe id="trade-wrapper" src="http://demo.actforex.sysfx.com:8100/trade/trade.jsp?entry=deal.184&lang=<?=$language;?>"></iframe>
 			<?php endif;?>
 			</div>
 		</div>
