7b362f9032dad91c7448201e05df41fd9dbc5e3a
Владимир <vkharseev@gmail.com>
2014-09-26 19:42:44 +0400

    link in top menu

--- application/views/users_interface/includes/top-menu.php
+++ application/views/users_interface/includes/top-menu.php
@@ -16,8 +16,40 @@
 					<li><?=anchor($main_menu[$i]['url'],$main_menu[$i]['link'],'class="top-link"');?></li>
 				<?php } ?>
 			<?php endfor;?>
+                <?php
+                $category_id = $page_id = FALSE;
+                $category_title = $page_link = FALSE;
+                for($i=0;$i<count($footer['pages']);$i++):
+                    if($footer['pages'][$i]['category'] == 23 && $this->language_url = 'en'):
+                        $category_id = 23;
+                    endif;
+                    if($footer['pages'][$i]['category'] == 22 && $this->language_url = 'ru'):
+                        $category_id = 22;
+                    endif;
+                    if($footer['pages'][$i]['category'] == 21 && $this->language_url = 'ind'):
+                        $category_id = 21;
+                    endif;
+                endfor;
+                if($category_id):
+                    for($i=0;$i<count($footer['category']);$i++):
+                        if ($footer['category'][$i]['id'] == $category_id):
+                            $category_title = $footer['category'][$i]['title'];
+                        endif;
+                    endfor;
+                    for($i=0;$i<count($footer['pages']);$i++):
+                        if ($footer['pages'][$i]['category'] == $category_id):
+                            $page_link = $footer['pages'][$i]['url'];
+                            break;
+                        endif;
+                    endfor;
+                endif;
+                ?>
+                <?php if($category_title && $page_link):?>
+                    <li><a class="top-link" href="<?=site_url($page_link);?>"><?=$category_title;?></a></li>
+                <?php else:?>
 					<li><a target="_blank" href="http://optospot.net/banners/trade_binery_options_<?php if($this->language_url == "ru") { ?>RUS<?php } else { ?>ENG<?php } ?>.pdf" class="top-link"><?=$this->localization->getLocalMessage('index','banner-right')?></a></li>
-			<?php
+                <?php endif;?>
+            <?php
 				/*switch ($this->language):
 					case '1': echo '<a target="_blank" class="top-link" href="'.baseURL('img/diploma.jpg').'">Awards</a>';break;
 					case '4': echo '<a target="_blank" class="top-link" href="'.baseURL('img/diploma.jpg').'">Penghargaan</a>';break;
